ceb387e050365acb792a41607c3a42ee1f841260
Suraj Shrestha <shresthasuraj62@gmail.com>
2019-02-28 11:09:04 -0600

    Add Two Texts from Input and Display it- Needs attention

--- responsive_button.js
+++ responsive_button.js
@@ -7,3 +7,11 @@ $("#button123").click(function(){
     var tElementReturns=document.getElementById("change12345");
     tElementReturns.innerHTML="This is what happens when you have jQuery Running."
 })
+
+$("#add_texts").click(function(){
+    var text1=documnet.getElementById("text111").value;
+    var text2=document.getElementById("text222").value;
+    var textAdd=text1+text2;
+    var dispalyHere=document.getElementById("display_add_text");
+    dispalyHere.innerHTML=textAdd;
+})
