56e55c806ea992868eae0ad31f75e5ac5670ca9f
Suraj Shrestha <shresthasuraj62@gmail.com>
2019-03-04 12:05:24 -0600

    Photo Show Effect-Button press to show image

--- responsive_button.js
+++ responsive_button.js
@@ -8,10 +8,10 @@ $("#button123").click(function(){
     tElementReturns.innerHTML="This is what happens when you have jQuery Running."
 })
 
-$("#add_texts").click(function(){
-    var text1=documnet.getElementById("text111").value;
+function addTexts(){
+    var text1=document.getElementById("text111").value;
     var text2=document.getElementById("text222").value;
     var textAdd=text1+text2;
-    var dispalyHere=document.getElementById("display_add_text");
-    dispalyHere.innerHTML=textAdd;
-})
+    var displayHere=document.getElementById("display_add_text");
+    displayHere.innerHTML=textAdd;
+}
